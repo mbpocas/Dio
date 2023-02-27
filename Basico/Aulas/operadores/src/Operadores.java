0e449d94758db5e83241da73a889f8ac1ba639d2
Mauricio Pocas <mbpocas@hotmail.com>
2023-02-27 20:18:56 -0300

    operadores

--- Basico/Aulas/operadores/src/Operadores.java
+++ Basico/Aulas/operadores/src/Operadores.java
@@ -1,7 +1,22 @@
 public class Operadores {
     public static void main(String[] args) throws Exception {
-      int numero = 5;
+     //qual o resultado das expressoes abaixo?
+      String concatenacao ="?";
+      System.out.println(concatenacao);
+      concatenacao = 1+1+1+"1";
+      System.out.println(concatenacao);
+      concatenacao = 1+"1"+1+1;
+      System.out.println(concatenacao);
+      concatenacao = 1+"1"+1+"1";
+      System.out.println(concatenacao);
+      concatenacao = "1"+1+1+1;
+      System.out.println(concatenacao);
+      concatenacao = "1"+(1+1+1);
+      System.out.println(concatenacao);
 
+
+      //Unários
+      int numero = 5;
       //Imprimindo o numero negativo
       System.out.println(- numero);
 
@@ -10,9 +25,9 @@ public class Operadores {
       System.out.println(numero);
 
       //incrementando numero em mais 1 numero, imprime 7
-      System.out.println(numero ++);// ops algo de errado não está certo
+      System.out.println(numero ++);// Imprimiu 6 e depois a variavel foi alterada para 7
 
-      System.out.println(numero);// agora sim, numero virou 7
+      System.out.println(numero);// agora sim, numero imprime 7
 
       //ordem de precedencia conta aqui
       System.out.println(++ numero);
@@ -20,5 +35,31 @@ public class Operadores {
       boolean verdadeiro = true;
 
       System.out.println("Inverteu " + !verdadeiro);
+
+
+      int a = 5;
+      int b = 4;
+      System.out.println(a==b);
+
+      String nome = "Mauricio";
+      String nome1 = "Mauricio";
+      String nome2 = new String("Mauricio"); // Criando novo objeto String - não será igual - false
+
+      System.out.println(nome==nome1);
+      System.out.println(nome1==nome2);
+
+      // para verificar o conteúdo usar equals() = verdadeiro
+      System.out.println(nome1.equals(nome2));
+
+      int numero1 = 1;
+      int numero2 = 1;
+
+
+      if(numero1== 1 & ++ numero2 == 2 )
+          System.out.println("As 2 condições são verdadeiras");
+
+      System.out.println("O numero 1 agora é " + numero1);
+      System.out.println("O numero 2 agora é " + numero2);
+
     }
 }

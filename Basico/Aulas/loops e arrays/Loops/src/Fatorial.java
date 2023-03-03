921df03ca4928cdd2e1c8a77fbc1cbdda1c2cab5
Mauricio Pocas <mbpocas@hotmail.com>
2023-03-03 18:49:54 -0300

    Ex6 - Fatorial

--- Basico/Aulas/loops e arrays/Loops/src/Fatorial.java
+++ Basico/Aulas/loops e arrays/Loops/src/Fatorial.java
@@ -7,12 +7,13 @@ import java.util.Scanner;
 public class Fatorial {
     public static void main(String[] args) {
         try (Scanner scan = new Scanner(System.in)) {
+
           System.out.println("Número: "); //Peça um número
           int numero = scan.nextInt(); //Entrada de dados, neste caso de um int.
 
           int multiplicacao = 1; //variável para guardar o valor da multiplicação dos valores.
 
-          System.out.print(numero + "! = "); //frase ilustrativo para indicar o fatorial
+          System.out.println(numero + "! = "); //frase ilustrativo para indicar o fatorial
 
           //para: variável i = numero, até i maior que 0, diminuimos 1 ao valor de i a cada loop.
           for(int i = numero; i > 0; i--) {
@@ -23,7 +24,7 @@ public class Fatorial {
                   System.out.print(i + "."); //imprima o valor do i seguido de "."
               } else { //se não (se o i tiver o valor igual a 1)
                   //imprima o valor do i seguido do sinal "=" e o resultado da multiplicação
-                  System.out.print(i + " = " + (multiplicacao));
+                  System.out.println(i + " = " + (multiplicacao));
               }
           }
         }
